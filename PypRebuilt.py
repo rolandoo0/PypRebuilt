51f2f462a6c59464a00b995d785432d13e55e8f0
Adrian Cruz <adr.ca@live.com>
2021-12-13 16:39:16 -0500

    setup

--- PypRebuilt.py
+++ PypRebuilt.py
@@ -8,57 +8,76 @@ try:
     import pyppeteer_stealth
 except:
     os.system("pip3 install pyppeteer_stealth")
-import asyncio
+
 from pyppeteer import launch
 from pyppeteer_stealth import stealth
 
 
 class PyppeteerRebuilt:
-    # Initialize the class by passing in a dictionary containing the headless key and
-    # a bool as the value as you would in the usual pyppeteer.launch() function
-    # i.e. main = PypRebuilt({'headless': True})
-    def __init__(self, args: dict):
+    def __init__(self, args: dict = {"headless": True}):
+        """
+        Initialize the class by passing in a dictionary containing the headless key and
+        a bool as the value as you would in the usual pyppeteer.launch() function
+        i.e. main = PypRebuilt({'headless': True})
+        """
         self.args = args
+        self.browser = None
 
-    # This function will a page defined by the url passed in as an argument
-    # ALL ASYNC functions MUST be awaited.
-    # Timeout is in milliseconds
-    # i.e. await main.loadPage("https://www.google.com", 1000)
-    async def loadPage(self, url: str):
+    async def loadPage(self, url: str, **kwargs):
+        """
+        This function will a page defined by the url passed in as an argument
+        ALL ASYNC functions MUST be awaited.
+        Timeout is in milliseconds
+        i.e. await main.loadPage("https://www.google.com", 1000)
+        """
         self.browser = await launch(self.args)
         self.page = await self.browser.newPage()
-        await self.page.goto(url)
+        await self.page.goto(url, **kwargs)
 
-    # This function will load the page using the stealth pyppeteer plugin
-    # i.e. await main.loadPageStealth("https://www.google.com")
-    async def loadPageStealth(self, url: str):
+    async def loadPageStealth(self, url: str, **kwargs):
+        """
+        This function will load the page using the stealth pyppeteer plugin
+        i.e. await main.loadPageStealth("https://www.google.com")
+        """
         self.browser = await launch(self.args)
         self.page = await self.browser.newPage()
         await stealth(self.page)
-        await self.page.goto(url)
+        await self.page.goto(url, **kwargs)
 
-    # This function will return the html of the page
-    # i.e. await main.getPageContent('https://www.google.com')
     async def getPageContent(self):
+        """
+        This function will return the html of the page
+        i.e. await main.getPageContent('https://www.google.com')
+        """
         return await self.page.content()
 
-    # This function will click a certain element on the page
-    # determined by a specific selector passed in as an argument
-    # i.e. await main.click('#search')
-    async def click(self, selector: str):
-        await self.page.click(selector)
+    async def click(self, selector: str, options={}, **kwargs):
+        """
+        This function will click a certain element on the page
+        determined by a specific selector passed in as an argument
+        i.e. await main.click('#search')
+        """
+        await self.page.click(selector, options, **kwargs)
 
-    # This function will execute a certain scrpit on the page
-    # i.e. await main.executeScript('document.querySelector("#search").value = "Hello World!"')
-    async def executeScript(self, script: str):
-        await self.page.evaluate(script)
+    async def executeScript(self, script: str, *args):
+        """
+        This function will execute a certain scrpit on the page
+        i.e. await main.executeScript('document.querySelector("#search").value = "Hello World!"')
+        """
+        await self.page.evaluate(script, *args)
 
-    # This function will return the value of a certain element on the page
-    # i.e. await main.getInnerText('#search')
-    async def getInnerText(self, selector: str):
-        return await self.page.querySelectorEval(selector, "node => node.textContent")
+    async def getInnerText(self, selector: str, *args):
+        """
+        This function will return the value of a certain element on the page
+        i.e. await main.getInnerText('#search')
+        """
+        return await self.page.querySelectorEval(
+            selector, "node => node.textContent", *args
+        )
 
-    # This function screenshots the current page
-    # i.e. await main.screenshot('screenshot.png')
-    async def screenShot(self, path: str):
-        await self.page.screenshot(path)
+    async def screenShot(self, options={}, **kwargs):
+        """
+        This function screenshots the current page
+        i.e. await main.screenshot('screenshot.png')
+        """
+        await self.page.screenshot(options, **kwargs)
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="PypRebuilt",
+    version="0.1.0",
+    description="A example Python package",
+    url="https://github.com/rolandoo0/PypRebuilt",
+    author="Sphere Enterprises",
+    author_email="",
+    license="MIT",
+)
